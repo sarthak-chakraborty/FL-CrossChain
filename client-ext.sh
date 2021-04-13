055a4803b88ae6ec2fd7cb04b8d3edd379a7b575
sarthak-chakraborty <sarthak.chakraborty@gmail.com>
2021-04-13 22:36:34 +0530

    VGG layers name added

--- client-ext.sh
+++ client-ext.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -rf /tmp/*
+sudo rm -rf /tmp/*
 docker rm --force $(docker ps -aq)
 export NETWORK=$1 
-docker run -dit --name alpine --network $1 alpine
+docker run -dit --name alpine --network $NETWORK alpine
 docker-compose -f docker-compose-client.yml up --build --scale client=$2 
 
 # tail -f nohup.out
--- federated/keras_models/VGG11.py
+++ federated/keras_models/VGG11.py
@@ -4,18 +4,18 @@ from tensorflow.keras import Input
 from tensorflow.keras.layers import Conv2D, BatchNormalization, MaxPool2D, ZeroPadding2D, Activation, Flatten, Dense
 
 
-def Fire(x, squeeze_planes, expand1x1_planes, expand3x3_planes):
+def Firei, (x, squeeze_planes, expand1x1_planes, expand3x3_planes):
 	# Squeeze
-	x = Conv2D(squeeze_planes, kernel_size=(1,1))(x)
-	x = Activation('relu')(x)
+	x = Conv2D(squeeze_planes, kernel_size=(1,1), name="conv-squeeze-{}".format(i))(x)
+	x = Activation('relu', "activation-squeeze-{}".format(i))(x)
 
 	# Expand
-	expand1x1 = Conv2D(expand1x1_planes, kernel_size=(1,1))(x)
-	expand1x1_act = Activation('relu')(expand1x1)
+	expand1x1 = Conv2D(expand1x1_planes, kernel_size=(1,1), name="conv-expand1-{}".format(i))(x)
+	expand1x1_act = Activation('relu', name="activation-expand1-{}".format(i))(expand1x1)
 
-	pad = ZeroPadding2D(padding=(1,1))(x)
-	expand3x3 = Conv2D(expand3x3_planes, kernel_size=(3,3))(pad)
-	expand3x3_act = Activation('relu')(expand3x3)
+	pad = ZeroPadding2D(padding=(1,1), name="zeropad-{}".format(i))(x)
+	expand3x3 = Conv2D(expand3x3_planes, kernel_size=(3,3), name="conv-expand2-{}".format(i))(pad)
+	expand3x3_act = Activation('relu', name="activation-expand2-{}".format(i))(expand3x3)
 
 	x = tf.keras.layers.add([expand1x1_act, expand3x3_act])
 
@@ -25,20 +25,20 @@ def Fire(x, squeeze_planes, expand1x1_planes, expand3x3_planes):
 def VGG(shape, num_classes, size=512):
 	tensor = Input(shape=shape)
 
-	x = Conv2D(8, kernel_size=(3, 3))(tensor)
-	x = BatchNormalization(momentum=0.1, epsilon=1e-5)(x)
-	x = Activation('relu')(x)
+	x = Conv2D(8, kernel_size=(3, 3), name="conv-initial")(tensor)
+	x = BatchNormalization(momentum=0.1, epsilon=1e-5, name="BN")(x)
+	x = Activation('relu', name="activation-initial")(x)
 
 	# Channels
 	layers = [32, 'M', 64, 'M', 128, 128, 'M', 128, 128, 'M', 128, 128, 'M']
 
-	for channels in layers:
+	for i, channels in enumerate(layers):
 		if channels == 'M':
-			x = MaxPool2D(pool_size=(2,2), strides=2)(x)
+			x = MaxPool2D(pool_size=(2,2), strides=2, name="pooling-{}".format(i))(x)
 		else:
-			x = Fire(x, int(channels/4), int(channels/2), int(channels/2))
-			x = BatchNormalization(momentum=0.1, epsilon=1e-5)(x)
-			x = Activation('relu')(x)
+			x = Fire(i, x, int(channels/4), int(channels/2), int(channels/2))
+			x = BatchNormalization(momentum=0.1, epsilon=1e-5, name="BN-{}".format(i))(x)
+			x = Activation('relu', name="activation-{}".format(i))(x)
 
 	# Final Layers
 	x = Flatten()(x)
